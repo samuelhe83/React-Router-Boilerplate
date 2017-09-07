c1acfc064c52fbebe507b10b978c72dc492c1490
Samuel He <samuelhe83@gmail.com>
2017-09-06 21:12:27 -0700

    Add images for slideshow as well as make CSS changes to make responsive

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import ImageGallery from 'react-image-gallery';
 import 'react-image-gallery/styles/css/image-gallery.css';
+import images from '../images.json';
 
 const Homepage = props => {
-  // <Slideshow images={['../download.jpeg', '../download2.jpeg', '../download3.jpeg']} />
-  let images = [
-    { original: '../download.jpeg', sizes: '5px', thumbnail: '../download.jpeg' },
-    { original: '../download2.jpeg' },
-    { original: '../download3.jpeg' }
-  ];
   return (
     <div className="homepage">
       <div className="banner">
         <p className="bannerText" id="first">
           For Your Aquarium
         </p>
-        <p className="bannerText"> No More Water Changing and No More Cleaning *</p>
+        <p className="bannerText"> No More Water Changing and No More Cleaning*</p>
         <p className="bannerText"> Eliminate Harmful Algal Bloom</p>
       </div>
       <div>
